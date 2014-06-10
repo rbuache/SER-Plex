36179cd212b897a10bf8339b86513a3b838d0181
Rui Reis <rui.reis@heig-vd.ch>
2014-06-10 12:52:48 +0200

    Corrections à XmlGenerateMedia.

--- Workspace/AdminComplexe/src/xml/XmlGenerateMedia.java
+++ Workspace/AdminComplexe/src/xml/XmlGenerateMedia.java
@@ -1,36 +1,24 @@
 package xml;
 
-import java.io.FileOutputStream;
-
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
 import launcher.Launcher;
+import net.sf.saxon.Transform;
 
 public class XmlGenerateMedia {
 
-	public XmlGenerateMedia() {
+	public XmlGenerateMedia(){
+
 
 	}
-
-	public boolean transform() {
+	public boolean transform(){
 		try {
-
-			TransformerFactory tFactory = TransformerFactory.newInstance();
-
-			Transformer transformer = tFactory
-					.newTransformer(new javax.xml.transform.stream.StreamSource(
-							"../../XSL/medialocal.xsl"));
-
-			transformer.transform(new javax.xml.transform.stream.StreamSource(
-					Launcher.databasePath + "/planification.xml"),
-					new javax.xml.transform.stream.StreamResult(
-							new FileOutputStream(
-									"../../XSL/media_generated/media.xml")));
+			
+			Transform.main(new String[]{"-s:" + Launcher.databasePath + "/planification.xml", 
+										"-xsl:../../XSL/medialocal.xsl",
+										"-o:../../XSL/media_generated/*"});
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
 		return true;
-
 	}
 }
--- XSL/medialocal.xsl
+++ XSL/medialocal.xsl
@@ -1,6 +1,6 @@
 ﻿<?xml version="1.0" encoding="UTF-8" ?>
-<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-<xsl:output name="page" method="html"      doctype-public="-//W3C//DTD HTML 4.01 Transitional//EN"/>
+<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:output name="page" method="xml"      doctype-public="-//W3C//DTD HTML 4.01 Transitional//EN"/>
 
 <!-- Différents fichiers utilisés -->
 <xsl:variable name="planif" select="document('../XML/planification.xml')"/>
@@ -8,6 +8,7 @@
 <xsl:variable name="location" select="document('../XML/location.xml')"/>
 
 <xsl:template match="/">
+	<xsl:result-document href="medialocal.xml" format="page">
 		<xsl:element name="medialocal">
 			<xsl:for-each select="$planif/planifications/movie">
 				<!-- on ne prend que les films diffusés à la date courante-->
@@ -23,6 +24,7 @@
 				</xsl:if>
 			</xsl:for-each>
 		</xsl:element>
+	</xsl:result-document>
 </xsl:template>
 
 <!--  template pour le nom du film-->
